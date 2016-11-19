207ccf5edd8e20e6e920f9fe570db08fcfc1ae6e
Jeremy Shepherd <jeremylshepherd@gmail.com>
2016-11-19 21:57:05 +0000

    Pruning more extraneous files, added screenshot to README

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Free Code Camp Voting Application
 Built using Express, MongoDB(Mongoose), React, React-router, Node.
 
 Register/Login using Github
+
+![ScreenShot](Screenshot 2016-11-19 16.49.55.png)
--- config/auth.js
+++ config/auth.js
@@ -8,24 +8,6 @@ module.exports = {
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
 		'callbackURL': process.env.APP_URL + 'auth/github/callback'
-	},
-
-    'googleAuth' : {
-        'clientID'         : process.env.GOOGLE_ID,
-        'clientSecret'     : process.env.GOOGLE_SECRET,
-        'callbackURL'      : process.env.APP_URL + 'auth/google/callback'
-    },
-    
-    'twitterAuth' : {
-        'consumerKey'         : process.env.TWITTER_CONSUMER,
-        'consumerSecret'     : process.env.TWITTER_SECRET,
-        'callbackURL'      : process.env.APP_URL + 'auth/twitter/callback'
-    },
-    
-    'facebookAuth' : {
-        'clientID'         : process.env.FACEBOOK_ID,
-        'clientSecret'     : process.env.FACEBOOK_SECRET,
-        'callbackURL'      : process.env.APP_URL + 'auth/facebook/callback'
-    }
+	}
 
 };
