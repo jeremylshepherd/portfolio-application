ed09891bfe81d3cb0e06d3ce396c0f559bdd2694
Jeremy Shepherd <jeremylshepherd@gmail.com>
2016-11-20 00:55:10 +0000

    Still troubleshooting Heroku deploy, Still Error R10 (Boot timeout) removed unused bCrypt

--- models/Users.js
+++ models/Users.js
@@ -2,7 +2,6 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
 
 var User = new Schema({
 	github: {
@@ -15,14 +14,5 @@ var User = new Schema({
         avatar: String
 	}
 });
-
-User.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-User.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
 
 module.exports = mongoose.model('User', User);
